df2ecfa76a9bbe7fca9f66ee49d4e4356f10d46c
Anibal Benalcazar <abenalcazar@KRUGER.COM.EC>
2019-12-05 10:52:57 -0500

    crear proyecto

---
+++ src/main/java/ec/com/empresa/cartshoping/CartShopingApplication.java
@@ -0,0 +1,13 @@
+package ec.com.empresa.cartshoping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartShopingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartShopingApplication.class, args);
+	}
+
+}
